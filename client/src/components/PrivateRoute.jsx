799d6582382a3edddd4b58ef4ab87e3301fa3522
deepak04iiitn <dky422003@gmail.com>
2024-07-03 22:28:01 +0530

    completed sidebar of the dashboard

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -7,5 +7,5 @@ export default function PrivateRoute() {
 
     const {currentUser} = useSelector((state) => state.user);
 
-  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;                                          // outlet will be pointing to the children of PrivateRote , i.e , dashboard
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;          // outlet will be pointing to the children of PrivateRote , i.e , dashboard
 }
