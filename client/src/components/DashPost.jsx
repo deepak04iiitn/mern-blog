832ccbe6e9b5eb57e991dc6906c599b5cf0938f2
deepak04iiitn <dky422003@gmail.com>
2024-07-07 01:30:39 +0530

    added show more functionality to the posts results of the dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -7,6 +7,7 @@ export default function DashPost() {
 
   const {currentUser} = useSelector(state => state.user);
   const [userPosts , setUserPosts] = useState([]);
+  const [showMore , setShowMore] = useState(true);
 
   useEffect(() => {
 
@@ -18,6 +19,11 @@ export default function DashPost() {
               if(res.ok)
               {
                   setUserPosts(data.posts);
+
+                  if(data.posts.length < 9)
+                  {
+                        setShowMore(false);
+                  }
               }
           } catch (error) {
               console.log(error.message);
@@ -31,9 +37,35 @@ export default function DashPost() {
 
   } , [currentUser._id])
 
+
+
+  const handleShowMore = async () => {
+
+        const startIndex = userPosts.length;
+
+        try {
+            
+            const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+            const data = await res.json();
+
+              if(res.ok)
+              {
+                  setUserPosts((prev) => [...prev , ...data.posts]);                      // keep the previous ones and add the new ones
+
+                  if(data.posts.length < 9)
+                    {
+                          setShowMore(false);
+                    }
+              }
+
+        } catch (error) {
+            console.log(error);
+        }
+  }
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
-        
+
         {currentUser.isAdmin && userPosts.length > 0 ? (
             <>
                 <Table hoverable className='shadow-md'>
@@ -87,6 +119,14 @@ export default function DashPost() {
                     ))}
 
                 </Table>
+
+                {
+                    showMore && (
+                        <button className='w-full text-teal-500 self-center text-sm py-7' onClick={handleShowMore}>
+                            Show more
+                        </button>
+                    )
+                }
             </>
         ) : (
             <p>You have no posts yet!</p>
